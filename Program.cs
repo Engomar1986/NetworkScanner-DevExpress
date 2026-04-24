45f54440d4e4e66a9f38521ab98dc3da70babf94
م.عمر الوريفي <88246734+Engomar1986@users.noreply.github.com>
2026-04-24 19:32:38 +0300

    Initialize Windows Forms application in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 
+using System;
+using System.Windows.Forms;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.Run(new Form1());
+    }
+}
