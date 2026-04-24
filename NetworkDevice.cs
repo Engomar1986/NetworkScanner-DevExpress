df8c1499d68f591f9b90558b3a2cbe133d8ab11b
م.عمر الوريفي <88246734+Engomar1986@users.noreply.github.com>
2026-04-24 19:35:40 +0300

    Add NetworkDevice class with properties

--- NetworkDevice.cs
+++ NetworkDevice.cs
@@ -1,1 +1,8 @@
-
+public class NetworkDevice
+{
+    public string IP { get; set; }
+    public string HostName { get; set; }
+    public string MAC { get; set; }
+    public string DeviceType { get; set; }
+    public string Status { get; set; }
+}
